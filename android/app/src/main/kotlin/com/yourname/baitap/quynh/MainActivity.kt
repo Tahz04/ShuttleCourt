b5671751eb27e236737a2a8c106162e3f72e330c
Dương Thành <122871406+Tahz04@users.noreply.github.com>
2026-04-25 19:40:53 +0700

    resolve branch confict

--- android/app/src/main/kotlin/com/yourname/baitap/quynh/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.yourname.baitap.quynh
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- quynh_backend/routes/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const bookingController = require('../controllers/bookingController');
-
-router.post('/', bookingController.createBooking);
-router.get('/user/:user_id', bookingController.getBookingsByUser);
-
-module.exports = router;
